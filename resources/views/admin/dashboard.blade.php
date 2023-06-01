6a07d21382aabbbbb9ae9a90781f54191d426ff1
AlessandroPagliuca <alessandro2ap3@gmail.com>
2023-06-01 16:57:01 +0200

    Upgraded dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,26 +1,115 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <h2 class="fs-4 text-secondary my-4">
-        {{ __('Dashboard') }}
-    </h2>
-    <div class="row justify-content-center">
-        <div class="col">
-            <div class="card">
-                <div class="card-header">{{ __('User Dashboard') }}</div>
+    <div class="container-fluid">
+        <h2 class="fs-4 text-secondary my-4">
+            {{ __('Dashboard') }}
+        </h2>
+    </div>
+    <div class="container-fluid d-flex">
+        <!--box right-->
+        <div class="w-50 p-2">
+            <!--First row top-left -->
+            <div class="row justify-content-start">
+                <div class="col-12 col-sm-6 col-md-4 ">
+                    <div class="card">
+                        <div class="card-header">{{ __('User Dashboard') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                    <div class="alert alert-success" role="alert">
-                        {{ session('status') }}
+                        <div class="card-body">
+                            @if (session('status'))
+                                <div class="alert alert-success" role="alert">
+                                    {{ session('status') }}
+                                </div>
+                            @endif
+
+                            {{ __('You are logged in!') }}
+                        </div>
                     </div>
-                    @endif
+                </div>
+                <div class="col-12 col-sm-6 col-md-4 ">
+                    <div class="card">
+                        <div class="card-header">{{ __('User Dashboard') }}</div>
 
-                    {{ __('You are logged in!') }}
+                        <div class="card-body">
+                            @if (session('status'))
+                                <div class="alert alert-success" role="alert">
+                                    {{ session('status') }}
+                                </div>
+                            @endif
+
+                            {{ __('You are logged in!') }}
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-sm-6 col-md-4 ">
+                    <div class="card">
+                        <div class="card-header">{{ __('User Dashboard') }}</div>
+
+                        <div class="card-body">
+                            @if (session('status'))
+                                <div class="alert alert-success" role="alert">
+                                    {{ session('status') }}
+                                </div>
+                            @endif
+
+                            {{ __('You are logged in!') }}
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--First row bottom-left -->
+            <div class="row justify-content-center">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-header">{{ __('User Dashboard') }}</div>
+                        <p>row 2</p>
+                        <div class="card-body">
+                            @if (session('status'))
+                                <div class="alert alert-success" role="alert">
+                                    {{ session('status') }}
+                                </div>
+                            @endif
+
+                            {{ __('You are logged in!') }}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--box right-->
+        <div class="w-50 p-2">
+            <div class="row flex-column justify-content-center">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-header">{{ __('User Dashboard') }}</div>
+                        <p>row 2</p>
+                        <div class="card-body">
+                            @if (session('status'))
+                                <div class="alert alert-success" role="alert">
+                                    {{ session('status') }}
+                                </div>
+                            @endif
+
+                            {{ __('You are logged in!') }}
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-header">{{ __('User Dashboard') }}</div>
+                        <p>row 2</p>
+                        <div class="card-body">
+                            @if (session('status'))
+                                <div class="alert alert-success" role="alert">
+                                    {{ session('status') }}
+                                </div>
+                            @endif
+
+                            {{ __('You are logged in!') }}
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
